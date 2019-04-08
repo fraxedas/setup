2ee3fda695fa9348a8b2e62d255f01f697367395
Oscar Fraxedas <fraxedas@gmail.com>
2019-04-08 13:07:35 -0400

    Windows install file

---
+++ windows.ps1
@@ -0,0 +1,23 @@
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install vscode
+choco install vscode -y
+
+# Notepadd++
+choco install notepadplusplus -y
+
+# postman
+choco install postman -y
+
+# fiddler
+choco install fiddler -y
+
+# git
+choco install git -y
+
+# kdiff3
+choco install kdiff3 -y
+
+# Upgrade
+choco upgrade all -y
