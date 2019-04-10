6805429dbaa36b95b992bade957af64ae2d0537b
Oscar Fraxedas <fraxedas@gmail.com>
2019-04-10 15:41:25 -0400

    MOAR git

--- git_clean.ps1
+++ git_clean.ps1
@@ -1,3 +1,10 @@
+$repo = $Args[0]
+Write-Output $repo
+if($repo){
+    Write-Output "Moving to: $repo"
+    Set-Location $repo
+}
+
 $current = $(git rev-parse --abbrev-ref HEAD)
 git fetch
 if ($current -ne 'master') {
@@ -5,4 +12,3 @@ if ($current -ne 'master') {
     git pull
     git branch -d $current
 }
-
