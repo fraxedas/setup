c417fd94108c94132ff4e14922e8bb8c98aabc4a
Oscar Fraxedas <fraxedas@gmail.com>
2019-04-10 15:30:24 -0400

    more git

--- git_clean.ps1
+++ git_clean.ps1
@@ -2,7 +2,7 @@ $current = $(git rev-parse --abbrev-ref HEAD)
 git fetch
 if ($current -ne 'master') {
     git checkout master
-    git delete $current
+    git pull
+    git branch -d $current
 }
-git pull
 
