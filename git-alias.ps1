6c44c2b532cee8a3719ba40d0daff757a124ca3d
Oscar Fraxedas <fraxedas@gmail.com>
2019-04-11 14:39:26 -0400

    more git

--- git-alias.ps1
+++ git-alias.ps1
@@ -1,3 +1,3 @@
 git config --global alias.branch-name 'rev-parse --abbrev-ref HEAD'
-git config --global alias.close-branch  '! [ $1 != master ] && git checkout master && git pull && git branch -d $1'
-git config --global alias.close  '! git fetch && git close-branch $(git branch-name) '
+git config --global alias.close-branch  '! [ $1 != master ] && echo cleaning branch $1 && git checkout master && git pull && git branch -d $1 '
+git config --global alias.close  '! git close-branch $(git branch-name) '
