b46afc9d7f453d2af964ef35546e41b03a5d78e4
Oscar Fraxedas <fraxedas@gmail.com>
2024-12-06 15:02:00 -0500

    Update git-alias.ps1
    git lg

--- git-alias.ps1
+++ git-alias.ps1
@@ -1,3 +1,4 @@
 git config --global alias.branch-name 'rev-parse --abbrev-ref HEAD'
 git config --global alias.close-branch  '! [ $1 != master ] && echo cleaning branch $1 && git checkout master && git pull && git branch -d $1 '
 git config --global alias.close  '! git close-branch $(git branch-name) '
+git config --global alias.lg  "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
