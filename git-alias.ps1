e1e60a5867e00c3dc73bd0a27683de9022e9acc1
Oscar Fraxedas <fraxedas@gmail.com>
2025-03-24 09:19:48 -0400

    Update git-alias.ps1

--- git-alias.ps1
+++ git-alias.ps1
@@ -1,4 +1,2 @@
-git config --global alias.branch-name 'rev-parse --abbrev-ref HEAD'
-git config --global alias.close-branch  '! [ $1 != master ] && echo cleaning branch $1 && git checkout master && git pull && git branch -d $1 '
-git config --global alias.close  '! git close-branch $(git branch-name) '
 git config --global alias.lg  "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
+git config --global alias.close '!f() { BR=$(git rev-parse --abbrev-ref HEAD); if [ "$BR" != "master" ]; then echo "[*] Current branch: $BR"; echo "[>] Switching to master..."; git checkout master && echo "[>] Pulling latest changes..."; git pull && echo "[>] Pruning remote-tracking branches..."; git fetch --prune && echo "[x] Deleting branch $BR..."; git branch -D "$BR"; else echo "[!] You are on master – not deleting it."; fi; }; f'
